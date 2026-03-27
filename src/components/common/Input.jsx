77f3f379a7a168ee0aed2642a9cadc3a0cd01607
sahana-crnr <sahana.s@getcarnera.com>
2026-03-27 18:08:24 +0530

    Delete src/components/common/Input.jsx

--- src/components/common/Input.jsx
+++
@@ -1,9 +0,0 @@
-export default function Input({ type, placeholder }) {
-    return (
-        <input
-            type={type}
-            placeholder={placeholder}
-            className="w-full border rounded-full px-4 py-2 outline-none"
-        />
-    );
-}
